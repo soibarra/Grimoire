0be9775a6cd1338f279bef97706dda5b01ad914d
Sofia Ibarra <sofiaquinn@Sofias-MacBook-Pro-3.local>
2026-04-28 12:56:03 -0700

    Add turnable medieval book pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import HTMLFlipBook from 'react-pageflip';
+
 import './App.css';
 
 function App() {
@@ -10,33 +12,79 @@ function App() {
 
         <h1>Grimoire</h1>
 
-        <div className="book">
+        <HTMLFlipBook
 
-          <div className="page left-page">
+          width={450}
+
+          height={560}
+
+          size="fixed"
+
+          minWidth={315}
+
+          maxWidth={900}
+
+          minHeight={400}
+
+          maxHeight={700}
+
+          showCover={true}
+
+          className="book"
+
+        >
+
+          <div className="page cover-page">
+
+            <h2>Grimoire</h2>
+
+            <p>A medieval spell book</p>
+
+          </div>
+
+          <div className="page">
 
             <h2>Chapter I</h2>
 
             <p>
 
-              This is where the left page content will go.
+              This is the first inside page of your grimoire.
 
             </p>
 
           </div>
 
-          <div className="page right-page">
+          <div className="page">
 
             <h2>The First Spell</h2>
 
             <p>
 
-              This is where the right page content will go.
+              This is where your spell description, story, or artwork can go.
 
             </p>
 
           </div>
 
-        </div>
+          <div className="page">
+
+            <h2>Notes</h2>
+
+            <p>
+
+              Add more magical notes, sketches, ingredients, or lore here.
+
+            </p>
+
+          </div>
+
+          <div className="page cover-page back-cover">
+
+            <h2>The End</h2>
+
+          </div>
+
+        </HTMLFlipBook>
 
       </section>
 
